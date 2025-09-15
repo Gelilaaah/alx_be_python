32a57af3992b86c3696d96305fff23c999a7008c
Gelila <gelila.melaku111@gmail.com>
2025-09-15 13:40:38 +0300

    Add basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+addition = number1 + number2
+subtraction = number2 - number1
+multiplication = number1 * number2
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Difference of {number2} and {number1} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
