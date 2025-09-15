15ab9710bf63da78587889c553ed49b9e92566d4
Gelilaaah <gelila.melaku111@gmail.com>
2025-09-15 14:11:14 +0300

    Add files via upload

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+print("Hello, Python file created successfully!")
+number1 = 10
+number2 = 5
+sum = number1 + number2
+difference = number2 - number1 
+product = number1 * number2
+print("Sum of ", number1, "and", number2, "is", sum)
+print("Difference of ", number1, "and", number2, "is", difference)
+print("Product of ", number1, "and", number2, "is", product)
+
---
+++ hours_to_seconds.py
@@ -0,0 +1,4 @@
+print("Hello, Python file created successfully!")
+hours = 2
+seconds = hours * 3600
+print(hours, "hour(s) is", seconds, " seconds")
